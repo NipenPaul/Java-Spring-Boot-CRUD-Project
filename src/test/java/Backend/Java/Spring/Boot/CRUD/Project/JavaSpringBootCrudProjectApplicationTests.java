8ab25c7c15e6991251395ace5fc01bc107e653bf
NipenPaul <nipenpaultpi27@gmail.com>
2025-04-21 10:42:42 +0600

    project setup mr

---
+++ src/test/java/Backend/Java/Spring/Boot/CRUD/Project/JavaSpringBootCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.Java.Spring.Boot.CRUD.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
